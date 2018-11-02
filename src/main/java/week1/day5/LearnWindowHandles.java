618308aa514373bc473ca192a35ebdf0eef2bc8d
sarath_M <kumar.testleaf@gmail.com>
2018-11-02 12:23:04 +0530

    week1.day 5 code

--- src/main/java/week1/day5/LearnWindowHandles.java
+++ src/main/java/week1/day5/LearnWindowHandles.java
@@ -21,10 +21,14 @@ public class LearnWindowHandles {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("https://legacy.crystalcruises.com/");
 		driver.findElementByLinkText("GUEST CHECK-IN").click();
+		// Returns the parent session ID
 		String parent = driver.getWindowHandle();
 		System.out.println(parent);
+		// Returns all the available windows
 		Set<String> allWindows = driver.getWindowHandles();
+		// create a list
 		List<String> list = new ArrayList<>();
+		// add all the windows to the list so that we can use get()
 		list.addAll(allWindows); 
 		driver.switchTo().window(list.get(1));
         System.out.println(driver.getTitle());
